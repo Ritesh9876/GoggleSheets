1799bcd0dcad0be153d28e53a27994cb82e7e0ae
Ritesh9876 <riteshmangdare@gmail.com>
2023-09-10 17:29:00 +0530

    UI layout complete: grid cells, actions bar

---
+++ script.js
@@ -0,0 +1,44 @@
+let rows = 100
+let cols = 26
+// infinite scrolling
+
+let addressColCont = document.querySelector(".address-col-cont");
+let addressRowCont = document.querySelector(".address-row-cont");
+let cellsCont = document.querySelector(".cells-cont");
+let addressBar = document.querySelector(".address-bar");
+
+// Adding first main vertical side column
+for (let i = 0;i < rows;i++) {
+    let addressCol = document.createElement("div");
+    addressCol.setAttribute("class", "address-col");
+    addressCol.innerText = i+1;
+    addressColCont.appendChild(addressCol);
+}
+
+// Adding top horizontal row
+for (let i = 0;i < cols;i++) {
+    let addressRow = document.createElement("div");
+    addressRow.setAttribute("class", "address-row");
+    addressRow.innerText = String.fromCharCode(65 + i);
+    addressRowCont.appendChild(addressRow);
+}
+
+// Adding all cells
+for (let i = 0;i < rows;i++) {
+    let rowCont = document.createElement("div");
+    rowCont.setAttribute("class", "row-cont");
+    for (let j = 0;j < cols;j++) {
+        let cell = document.createElement("div");
+        cell.setAttribute("class", "cell");
+        cell.setAttribute("contenteditable", "true");
+        cell.setAttribute("spellcheck", "false");
+
+        // Attributes for cell and storage identification
+        cell.setAttribute("rid", i);
+        cell.setAttribute("cid", j);
+
+        rowCont.appendChild(cell);
+       // addListenerForAddressBarDisplay(cell, i, j);
+    }
+    cellsCont.appendChild(rowCont);
+}
